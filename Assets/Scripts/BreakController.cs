99b25c845fd177d10d5e5a9fc8ba4569c8c52152
Chris Lee <chris0803lee@gmail.com>
2023-07-23 16:04:24 +0900

    Update BreakController.cs
    Get rid of secondary function.
    The explosion force only affects the broken object instead of whatever's around it.

--- Assets/Scripts/BreakController.cs
+++ Assets/Scripts/BreakController.cs
@@ -4,16 +4,10 @@ using UnityEngine;
 
 public class BreakController : MonoBehaviour
 {
-    public HouseController houseCon;
-
-    public bool secondary;
-
     public GameObject hitEffect;
-
     public GameObject broken;
     public float radius;
     public float force;
-    Renderer weaponRenderer;
 
     bool spawned = false;
 
@@ -22,11 +16,7 @@ public class BreakController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        houseCon = GameObject.FindWithTag("World").GetComponent<HouseController>();
-
         spawned = false;
-
-        weaponRenderer = transform.GetChild(0).GetComponent<Renderer>();
 
         santaThrowColliderLayer = LayerMask.NameToLayer("Santa Throw Collider");
         arrowLayer = LayerMask.NameToLayer("Arrow");
@@ -43,22 +33,11 @@ public class BreakController : MonoBehaviour
 
         GameObject child = gameObject.transform.GetChild(0).gameObject;
         GameObject spawnedBroken = Instantiate(broken, child.transform.position, child.transform.rotation);
-        spawnedBroken.transform.parent = houseCon.currentHouse.transform;
 
-        if (secondary) {
-            spawnedBroken.transform.localScale = transform.parent.localScale;
-        } else {
-            spawnedBroken.transform.localScale = child.transform.localScale;
-            Collider[] colliders = Physics.OverlapSphere(hitPoint, radius);
-            
-            foreach (Collider nearbyObject in colliders) {
-                if (nearbyObject.gameObject.layer == LayerMask.NameToLayer("Pieces")) {
-                    Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
-
-                    if (rb != null) {
-                        rb.AddExplosionForce(force, hitPoint, radius);
-                    }
-                }
+        spawnedBroken.transform.localScale = child.transform.localScale;
+        foreach (Rigidbody rb in spawnedBroken.GetComponentsInChildren<Rigidbody>()) {
+            if (rb != null) {
+                rb.AddExplosionForce(force, hitPoint, radius);
             }
         }
 
@@ -80,8 +59,6 @@ public class BreakController : MonoBehaviour
                 if (mag > 0) {
                     Break(transform.position, collisionInfo.transform.position);
                 }
-
-                //collisionInfo.gameObject.GetComponent<ArrowController>().DestroyArrow();
             }
         }
     }
