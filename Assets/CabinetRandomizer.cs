d4885cc3216fe56920a045f585575d62bbe1bbb1
Chris Lee <chris0803lee@gmail.com>
2023-07-28 16:08:25 +0900

    Randomized the number of jars in cabinet

---
+++ Assets/CabinetRandomizer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CabinetRandomizer : MonoBehaviour
+{
+    int numOfChildren, destroyAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numOfChildren = transform.childCount;
+        destroyAmount = 4;
+        for (int i = 0; i < numOfChildren / destroyAmount; i++) {
+            transform.GetChild(Random.Range(1, numOfChildren)).gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
